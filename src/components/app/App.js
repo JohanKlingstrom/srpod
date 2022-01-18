64430aa6afc85638efa74f079f458cc3bcf244fa
johk <johkli19@hotmail.se>
2022-01-18 12:27:48 +0100

    Readme

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>sr<span className="highlight">Pod</span></h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
